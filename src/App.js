a83e2f103bf9b41950f73538f325aba1e0bafc1f
Tim Allon <timallon@gmail.com>
2022-11-26 17:39:31 +0000

    basic signup form added but not connecting to server - first attempt rendering of separate react routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Login from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import Search from "./Components/Search";
 import Signup from "./Components/Signup";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
 
   return (
     <div className="App">
-      <Navbar />
-      <Login />
-      <Signup />
-      <Search />
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/signup" element={ <Signup /> } />
+        <Route path="/" element={ <Navbar /> } />
+        <Route path="/search" element={ <Search /> } />
+      </Routes>
       
     </div>
   );
