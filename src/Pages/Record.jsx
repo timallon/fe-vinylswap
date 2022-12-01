b2d239b0b98ef111a8a72845501353e42987d197
Tim Allon <timallon@gmail.com>
2022-12-01 15:56:14 +0000

    comment mapping functional

--- src/Pages/Record.jsx
+++ src/Pages/Record.jsx
@@ -2,15 +2,17 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 
 import { Link, useParams } from "react-router-dom"; // <== IMPORT 
-const { REACT_APP_MY_ENV } = process.env; 
- 
- 
-function Record (props) {
+const { REACT_APP_MY_ENV } = process.env;
+
+
+function Record(props) {
   const [record, setRecord] = useState(null);
   // Get the URL parameter `:projectId` 
   const { recordId } = useParams();            // <== ADD
-  
-  
+  console.log("the o0ne", record)
+  console.log(recordId)
+
+
   // Helper function that makes a GET request to the API
   // and retrieves the project by id
   const getRecord = () => {          //  <== ADD A NEW FUNCTION
@@ -18,18 +20,17 @@ function Record (props) {
       .get(`${REACT_APP_MY_ENV}/records/${recordId}`)
       .then((response) => {
         const oneRecord = response.data;
-        console.log(oneRecord)
         setRecord(oneRecord);
       })
       .catch((error) => console.log(error));
   };
-  
-  
-  useEffect(()=> {                   // <== ADD AN EFFECT
+
+  useEffect(() => {                   // <== ADD AN EFFECT
     getRecord();
-  }, [] );
- 
-  
+  }, []);
+
+
+
   return (
     <div className="Record">
       {record && (
@@ -39,11 +40,22 @@ function Record (props) {
           <p>Originally Released: {record.yearReleased}</p>
           <p>Record Label: {record.label}</p>
           <p>Genre: {record.genre}</p>
-          <img src={record.image} alt="Record Cover"/>
-          
+          <img src={record.image} alt="Record Cover" />
         </>
       )}
- 
+          <form>
+            <textarea rows="3" type="text" name="description" placeholder="Add a comment." />
+            <button type="submit">Submit</button>
+          </form>
+
+          {record.comment.map((commentArray) => {
+            return (
+              <div>
+                <h3>{commentArray.description}</h3>
+              </div>
+            )
+          })}
+
       {record &&
         (
           <li className="Record card" key={record._id}>
@@ -52,16 +64,16 @@ function Record (props) {
             <p>{record.artist}</p>
           </li>
         )}
- 
+
       <Link to="/records">
         <button>Back to records</button>
       </Link>
-      
+
       {/*    ADD    */}
       <Link to={`/records/${recordId}/update`}>
         <button>Edit Record</button>
-      </Link>      
-      
+      </Link>
+
     </div>
   );
 }
